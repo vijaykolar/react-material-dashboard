e7d6ccee6b3cbff03c10965c0452177e5d141897
Vijay Kolar <vijayikolar@gmail.com>
2021-03-20 10:34:26 +0530

    Added search drawer and lazy loading

--- src/App/Header/Navigation/Search/Search.js
+++ src/App/Header/Navigation/Search/Search.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { IconButton, Tooltip } from "@material-ui/core";
 import { HiSearch } from "react-icons/all";
+import loadable from "@loadable/component";
+
+const LoadableDrawer = loadable(() => import("./Drawer"));
 
 const Search = () => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const openDrawerHandler = () => {
+    setOpenDrawer(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setOpenDrawer(false);
+  };
+
+  const inputChangeHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <Tooltip title="Search">
-      <IconButton color="inherit" aria-label="Search">
-        <HiSearch />
-      </IconButton>
-    </Tooltip>
+    <>
+      <Tooltip title="Search">
+        <IconButton
+          onClick={openDrawerHandler}
+          color="inherit"
+          aria-label="Search"
+        >
+          <HiSearch />
+        </IconButton>
+      </Tooltip>
+
+      <LoadableDrawer
+        search={search}
+        openDrawer={openDrawer}
+        closeDrawerHandler={closeDrawerHandler}
+        inputChangeHandler={inputChangeHandler}
+      />
+    </>
   );
 };
 
