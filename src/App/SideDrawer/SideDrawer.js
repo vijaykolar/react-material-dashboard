846455aeab28b3e7cbecc10098346298fe481984
Vijay Kolar <vijayikolar@gmail.com>
2021-03-20 15:26:32 +0530

    Working on side drawer

---
+++ src/App/SideDrawer/SideDrawer.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Drawer,
+  Box,
+  Hidden,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+
+import styled from "styled-components";
+
+const StyledBox = styled(Box)`
+  width: 240px;
+
+  @media (min-width: 960px) {
+    margin-top: 64px;
+  }
+`;
+
+const SideDrawerBox = styled(Box)`
+  width: 240px;
+  flex-shrink: 0;
+  height: 100vh;
+`;
+
+function SideDrawer(props) {
+  const dummyCategories = [
+    "Hokusai sss",
+    "Hiroshige",
+    "Utamaro",
+    "Kuniyoshi",
+    "Yoshitoshi",
+  ];
+
+  const theme = useTheme();
+
+  const drawer = (
+    <StyledBox>
+      <List>
+        {dummyCategories.map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </StyledBox>
+  );
+  return (
+    <SideDrawerBox>
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Hidden smUp implementation="css">
+        <Drawer
+          variant="temporary"
+          anchor={theme.direction === "rtl" ? "right" : "left"}
+          open={props.mobileOpen}
+          onClose={props.handleDrawerToggle}
+          ModalProps={{
+            keepMounted: false, // Better open performance on mobile.
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+      <Hidden xsDown implementation="css">
+        <Drawer variant="permanent"> {drawer}</Drawer>
+      </Hidden>
+    </SideDrawerBox>
+  );
+}
+SideDrawer.propTypes = {
+  // Injected by the documentation to work in an iframe.
+  // You won't need it on your project.
+  container: PropTypes.object,
+};
+export default SideDrawer;
