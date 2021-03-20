9e249966a2733ef83c0f5e01f6b352ac5dbf7320
Vijay Kolar <vijayikolar@gmail.com>
2021-03-20 15:24:56 +0530

    Working on side drawer

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton } from "@material-ui/core";
 import styled from "styled-components";
+import { HiMenuAlt4 } from "react-icons/all";
+
 import Navigation from "./Navigation/Navigation";
 import Logo from "./Logo/Logo";
 
@@ -8,10 +10,24 @@ const Div = styled.div`
   flex-grow: 1;
 `;
 
-const Header = () => {
+const StyledIconButton = styled(IconButton)`
+  @media (min-width: 960px) {
+    display: none;
+  }
+`;
+
+const Header = (props) => {
   return (
-    <AppBar elevation={0}>
+    <AppBar elevation={0} style={{ zIndex: 1201 }}>
       <Toolbar>
+        <StyledIconButton
+          onClick={props.onClick}
+          edge="start"
+          color="inherit"
+          aria-label="Menu"
+        >
+          <HiMenuAlt4 />
+        </StyledIconButton>
         <Logo />
         <Div />
         <Navigation />
