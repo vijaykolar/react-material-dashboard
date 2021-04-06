119068582a804b9a795c1773213482b6d30cc949
Vijay Kolar <vijayikolar@gmail.com>
2021-04-06 10:20:05 +0530

    Cost Breakdown

--- src/App/Finance/components/CostBreakdown.js
+++ src/App/Finance/components/CostBreakdown.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Paper, Typography } from "@material-ui/core";
+import Padding from "../../hoc/Padding";
+import CostBreakdownRadialChart from "./CostBreakdownRadialChart";
 
 const CostBreakdown = () => {
-  return <div>CostBreakdown</div>;
+  return (
+    <Paper variant="outlined" elevation={0}>
+      <Padding>
+        <Typography variant="h6">Cost Breakdown</Typography>
+      </Padding>
+      <CostBreakdownRadialChart />
+    </Paper>
+  );
 };
 
 export default CostBreakdown;
--- src/App/Finance/components/SalesRevenueLineChart.js
+++ src/App/Finance/components/SalesRevenueLineChart.js
@@ -8,15 +8,15 @@ class SalesRevenueLineChart extends React.Component {
     this.state = {
       series: [
         {
-          name: "Session Duration",
+          name: "Sales",
           data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10],
         },
         {
-          name: "Page Views",
+          name: "Cost",
           data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
         },
         {
-          name: "Total Visits",
+          name: "Profit",
           data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47],
         },
       ],
@@ -110,7 +110,7 @@ class SalesRevenueLineChart extends React.Component {
 
   render() {
     return (
-      <div id="chart">
+      <div>
         <ReactApexChart
           options={this.state.options}
           series={this.state.series}
